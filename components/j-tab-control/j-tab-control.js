74719262272e3961fd0393c4e8a1e7d9579186cd
jixiang <zhangjx@dsbz.com>
2021-07-15 19:47:42 +0800

    组件化开发

---
+++ components/j-tab-control/j-tab-control.js
@@ -0,0 +1,36 @@
+// components/j-tab-control/j-tab-control.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    titles: {
+      type: Array,
+      value: []
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    currentIndex: 0
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+    handleItemClick(event) {
+      //取出index
+      const index = event.currentTarget.dataset.index;
+      //2修改currentIndex
+      this.setData({
+        currentIndex: index
+      })
+      //通知页面内部的点击事件
+      this.triggerEvent('itemclick', {index, title: this.properties.titles[index]}, {})
+
+    }
+  }
+})
--- home/home.js
+++ home/home.js
@@ -5,7 +5,28 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    counter: 0
+  },
+  handleIncrement(event) {
+    console.log("----------", event)
+    this.setData({
+      counter: this.data.counter + 1
+    })
+  },
+  handleTabClick(event){
+    console.log(event)
+  },
+  handleIncrementCpn(){
+    //最终目的：修改my-sel的counter
+    //1.获取组件对象
+    const my_sel = this.selectComponent('#sel-id')
+    // console.log(my_sel)
+    //2.通过setData修改组件中的数据（不合理）
+    // my_sel.setData({
+    //   counter: my_sel.data.counter + 20
+    // })
+    //3.通过方法对数据进行修改
+    my_sel.incrementCounter(10)
   },
 
   /**
